6cd235d29f5d40675c759d2f7076a489270910e5
Trịnh Mạnh Cường <cuong.trinh@jellyfishhr.com>
2017-12-09 21:17:19 +0900

    dash board and routing make

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//import routing module
+import { AppRoutingModule } from './app.router';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
