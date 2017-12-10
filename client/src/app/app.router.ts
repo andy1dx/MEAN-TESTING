fdf2a11d7994f659cfac384b8fa66a39a94fe5bb
Trịnh Mạnh Cường <cuong.trinh@jellyfishhr.com>
2017-12-10 14:14:57 +0900

    register form and validation

--- client/src/app/app.router.ts
+++ client/src/app/app.router.ts
@@ -4,10 +4,13 @@ import { NgModule } from '@angular/core';
 
 //import component module
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { RegisterComponent } from './component/register/register.component';
 
 const AppRoutes:Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: DashboardComponent },
 ];
 
